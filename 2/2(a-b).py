695650e1e44e8960955501333e0be5e111e51d8f
Bulxyz <60338320+Bulxyz@users.noreply.github.com>
2024-04-15 20:39:29 +0330

    Create 2(a-b).py

---
+++ 2/2(a-b).py
@@ -0,0 +1,16 @@
+a) For double-precision floating-point numbers (floats) in Python, the underflow and overflow limits in decimal are as follows:
+
+
+Underflow occurs when the number is too small to be represented accurately. In Python, the underflow limit for double-precision floating-point numbers is approximately 2.2250738585072014e-308.
+
+
+Overflow occurs when the number is too large to be represented accurately. In Python, the overflow limit for double-precision floating-point numbers is approximately 1.7976931348623157e+308.
+
+
+b) For double-precision floating-point numbers (doubles) in Python, the underflow and overflow limits in decimal are as follows:
+
+
+Underflow occurs when the number is too small to be represented accurately. In Python, the underflow limit for double-precision floating-point numbers is approximately 4.9406564584124654e-324.
+
+
+Overflow occurs when the number is too large to be represented accurately. In Python, the overflow limit for double-precision floating-point numbers is approximately 1.7976931348623157e+308.
