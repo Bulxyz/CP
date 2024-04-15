0a2b78bf1c96af6b70ddcb48167473ad5b12da7f
Bulxyz <60338320+Bulxyz@users.noreply.github.com>
2024-04-15 20:30:44 +0330

    Create 1(d).py

---
+++ 2/1(d).py
@@ -0,0 +1,18 @@
+To determine the full value of A based on the provided bit positions and values:
+
+
+
+For s (bit positions 31), the value is 0.
+
+For e (bit positions 30 to 23), the value is 0000 1110.
+
+For f (bit positions 22 to 0), the value is 1010 0000 0000 0000 0000 000.
+
+
+Now, let's combine these values to determine the full value of A:
+
+
+A = 0 0000 1110 1010 0000 0000 0000 0000 0000 000
+
+
+Therefore, the full value of A is 00001110101000000000000000000000 in binary.
