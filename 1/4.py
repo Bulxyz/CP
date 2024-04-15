780b5b1773058eecb0a00f3ad7e678c75ef09162
Bulxyz <60338320+Bulxyz@users.noreply.github.com>
2024-04-15 20:02:30 +0330

    Update 4.py

--- 1/4.py
+++ 1/4.py
@@ -1,1 +1,12 @@
+# Area.py
 
+import math
+
+PI = 3.14159
+
+def volume_of_sphere(radius):
+  return (4/3) * PI * radius ** 3
+
+radius = float(input("Enter the radius of the sphere: "))
+volume = volume_of_sphere(radius)
+print("The volume of the sphere is:", volume)
